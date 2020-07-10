8e9989348ccbe7c71599fa116db1adc32e0d868d
zym2018112558 <1762114083@qq.com>
2020-07-10 16:19:29 +0800

    开始

---
+++ PSMS.DAL/User.DAL.cs
@@ -0,0 +1,22 @@
+﻿using PSMS.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSMS.DAL
+{
+    public class Class1
+    {
+       //alt+回车引用类库
+       //在App.config里添加数据库连接字符串
+       //软删除
+       //添加maysql.data引用
+
+
+
+
+        //访问数据库
+    }
+}
