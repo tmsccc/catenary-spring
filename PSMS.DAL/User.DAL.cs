a397e324078d839c7fc9433cd015e25f92c723dd
tianmaosong_2018112722 <2831193565@qq.com>
2020-07-10 20:06:06 +0800

    添加类库

--- PSMS.DAL/User.DAL.cs
+++ PSMS.DAL/User.DAL.cs
@@ -4,19 +4,127 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+using PSM;
 
-namespace PSMS.DAL
+namespace PMS15.DAL
 {
-    public class Class1
-    {
-       //alt+回车引用类库
-       //在App.config里添加数据库连接字符串
-       //软删除
-       //添加maysql.data引用
+	class UserDAL
+	{
+		SqlDbHelper db = new SqlDbHelper();
+		//添加员工信息
+		public int AddUser(User user)
+		{
+			//sql语句
+			String sql = "INSERT INTO t_user(emp_id,emp_name,emp_sex,emp_job,emp_entrytime,emp_mailbox,emp_department,Is_del) VALUES(@emp_id,@emp_name,@emp_sex,@emp_job,@emp_entrytime,@emp_mailbox,@emp_department,@Is_del)";
+			//参数列表
+			MySqlParameter[] param = {
+				 new MySqlParameter("@emp_id",MySqlDbType.Int32),
+				 new MySqlParameter("@emp_name",MySqlDbType.VarChar),
+				 new MySqlParameter("@emp_sex",MySqlDbType.VarChar),
+				 new MySqlParameter("@emp_job",MySqlDbType.VarChar),
+				 new MySqlParameter("@emp_entrytime",MySqlDbType.DateTime),
+				 new MySqlParameter("@emp_mailbox",MySqlDbType.VarChar),
+				 new MySqlParameter("@emp_department",MySqlDbType.VarChar),
+				 new MySqlParameter("@emp_department",MySqlDbType.Int16),
+
+			};
+			param[0].Value = user.id;
+			param[1].Value = user.name;
+			param[2].Value = user.sex;
+			param[3].Value = user.job;
+			param[4].Value = user.entrytime;
+			param[5].Value = user.email;
+			param[6].Value = user.department;
+			param[7].Value = user.is_del;
+
+			return db.ExecuteNonQuery(sql, param);
+
+		}
+		//更新用户信息
+		public int UpdateUser(User user)
+		{
+			String sql = "UPDATE t_user SET emp_name=@emp_name,emp_sex=@emp_sex,emp_job=@emp_job,emp_entrytime=@emp_entrytime,emp_department=@emp_department WHILE emp_id=@emp_id";
+			//参数列表
+			MySqlParameter[] param ={
+			  	 new MySqlParameter("@emp_id",MySqlDbType.Int32),
+				 new MySqlParameter("@emp_name",MySqlDbType.VarChar),
+				 new MySqlParameter("@emp_sex",MySqlDbType.VarChar),
+				 new MySqlParameter("@emp_job",MySqlDbType.VarChar),
+				 new MySqlParameter("@emp_entrytime",MySqlDbType.DateTime),
+				 new MySqlParameter("@emp_mailbox",MySqlDbType.VarChar),
+				 new MySqlParameter("@emp_department",MySqlDbType.VarChar),
+				 new MySqlParameter("@emp_department",MySqlDbType.Int16),
+
+			};
+			//参数赋值
+			param[0].Value = user.id;
+			param[1].Value = user.name;
+			param[2].Value = user.sex;
+			param[3].Value = user.job;
+			param[4].Value = user.entrytime;
+			param[5].Value = user.email;
+			param[6].Value = user.department;
+			param[7].Value = user.is_del;
+			return db.ExecuteNonQuery(sql, param);
+		}
+		//软删除，Is_del字段如果为——0，则用户不存在
+		public int SoftDelete(int id)
+		{
+			String sql = "UPDATE t_uesr SET Is_del=1 WHERE emp_id=" + id;
+			return db.ExecuteNonQuery(sql);
+		}
+		//硬删除
+		public int RealDelete(int id)
+		{
+			String sql = "DELETE FROM t_user WHERE emp_id=" + id;
+			return db.ExecuteNonQuery(sql);
+		}
+		//查询所有的员工，以链表的形式返回所有数据
+		public List<User> GetAllUsers(int is_Del) //is_Del字段是显示员工是否被删除，is_Del=1可以查询所有在职员工，is_Del=1则查询删除的员工
+		{
+			String sql = "SELECT *FROM t_user WHERE Is_del=" + is_Del;
+			DataTable dt = db.ExecuteDataTable(sql);
+			List<User> users = new List<User>();
+			foreach (DataRow dr in dt.Rows)
+			{
+				//行转化成对象
+				User user = DataRowToUser(dr);
+				users.Add(user);
+			}
+			return users;
+
+		}
+		//以DataTable的形式返回数据
+		public DataTable GetAllUsers()
+		{
+			int is_Del = 0;
+			String sql = "SELECT * FROM t_user WHERE Is_del=" + is_Del;
+			DataTable dt = db.ExecuteDataTable(sql);
+			return dt;
+		}
+		//从datable中获取数据
+		private User DataRowToUser(DataRow dr)
+		{
+			User user = new User();
+
+			//user.Id = Convert.ToInt32(dr[0]);
+			user.id = Convert.ToInt32(dr["emp_id"]);
+			if (dr["entry_time"] != DBNull.Value)//判断日期非空
+			{
+				user.entrytime = Convert.ToDateTime(dr["emp_entrytime"]);
+			}
+			user.name = Convert.ToString(dr["emp_name"]);
+			user.sex = Convert.ToString(dr["emp_sex"]);
+			user.job = Convert.ToString(dr["emp_job"]);
+			user.email = Convert.ToString(dr["emp_email"]);
+			user.is_del = Convert.ToInt32(dr["Is_del"]);
+			user.department = Convert.ToString(dr["emp_department"]);
+			return user;
+		}
 
 
-
-
-        //访问数据库
-    }
+	}
 }
---
+++ PSMS.Model/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSMS.Model
+{
+	public class Account //用户账号信息类
+	{
+		public int account { set; get; }//用户账号
+		public String password { set; get; }//用户密码
+		public int id { set; get; }//用户编号
+								   //无参构造函数
+		public Account() { }
+		//带参构造函数
+		public Account(int id, int account, String password)
+		{
+			this.id = id;
+			this.account = account;
+			this.password = password;
+		}
+	}
+}
