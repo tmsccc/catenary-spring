a6a02bc6cc2f5d878515584c8f26676aa10b62fb
zym2018112558 <1762114083@qq.com>
2020-07-10 21:24:56 +0800

    用户逻辑

--- PSMS.DAL/User.DAL.cs
+++ PSMS.DAL/User.DAL.cs
@@ -4,11 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using MySql.Data;
+
 
 namespace PSMS.DAL
 {
     public class Class1
-    {
+    {  //using System.Data;
        //alt+回车引用类库
        //在App.config里添加数据库连接字符串
        //软删除
